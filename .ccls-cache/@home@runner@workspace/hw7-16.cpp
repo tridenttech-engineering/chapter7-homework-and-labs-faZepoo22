8ab684b357ec99413914f2430f3609ec831a0cfa
faze poo <39823171-crony-upland7v@users.noreply.replit.com>
2025-03-19 20:52:29 +0000

    retry homework7
    re-commit, I don't believe it worked the first time.

---
+++ .ccls-cache/@home@runner@workspace/hw7-16.cpp
@@ -0,0 +1,31 @@
+// hw7-16.cpp - displays three tip amounts
+// Created/revised by <your name> on <current date>
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+		double bill = 0.0;
+		double tip = 0.0;
+		double rate = 0.1; 
+
+		cout << "Bill amount: ";
+		cin >> bill;
+
+		cout << fixed;
+
+		while (rate <= 0.2)  
+		{
+				tip = bill * rate;
+				cout << setprecision(0);
+				cout << rate * 100 << "% tip: ";
+				cout << setprecision(2);
+				cout << "$" << tip << endl;
+
+				rate += 0.05; 
+		}   
+
+		return 0;
+}   
